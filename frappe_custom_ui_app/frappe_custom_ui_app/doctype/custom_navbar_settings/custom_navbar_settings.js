1b49989a0caa894159020cafe70005a3579f23fb
janakvaghela542 <janak@pseudocode.co>
2025-10-03 18:51:28 +0530

    Navbar Dymanic Color Changing.

---
+++ frappe_custom_ui_app/frappe_custom_ui_app/doctype/custom_navbar_settings/custom_navbar_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Janak Vaghela and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Custom Navbar Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
